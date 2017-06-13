8b6fdd4f35300af232b493f5ace625bc3e0565fe
Michell Cronberg <michell@cronberg.dk>
2017-06-13 15:23:25 +0200

    Nåede til viewbag

---
+++ Project03_Controllers/Controllers/AppController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Project03_Controllers.Controllers
+{
+    public class AppController : Controller
+    {
+        [Route("~/")]
+        public ActionResult Index()
+        {
+            //return View();
+
+            //ContentResult c = new ContentResult();
+            //c.Content = "<h1>Test</h1>";
+            ////return Content("<h1>Test</h1>");
+            //return c;
+
+            //HttpStatusCodeResult h = new HttpStatusCodeResult(500, "FEJL FEJL");
+            //return h;
+
+            var lst = new PersonNuGetPackage.PersonRepositoryRandom().GetPeople(15);
+            //return Json(lst, JsonRequestBehavior.AllowGet);
+
+            //return Redirect("http://www.google.dk");
+
+
+            ViewBag.time = DateTime.Now.ToLongTimeString();
+            ViewBag.people = lst; 
+
+            return View();
+
+        }
+    }
+}
---
+++ Project03_Controllers/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Project03_Controllers.MvcApplication" Language="C#" %>
