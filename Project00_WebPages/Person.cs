3257b4b0f0567c1023e7ad652c780c83c384ccc6
Michell Cronberg <michell@cronberg.dk>
2017-06-13 11:34:37 +0200

    .

---
+++ Project00_WebPages/Person.cs
@@ -0,0 +1,16 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Project00_WebPages
+{
+    public class Person
+    {
+
+        public static string Test() {
+            return Guid.NewGuid().ToString();
+        }
+    }
+}
