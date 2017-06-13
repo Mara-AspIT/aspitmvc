bc328ffc68777ad9a2d10b50ec1a8475e345a8ec
Michell Cronberg <michell@cronberg.dk>
2017-06-13 13:32:19 +0200

    .

---
+++ Project02_Models/Controllers/AppController.cs
@@ -0,0 +1,48 @@
+﻿using Project02_Models.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Project02_Models.Controllers
+{
+    public class AppController : Controller
+    {
+        [Route("~/")]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        [Route("~/Login")]
+        public ActionResult Login()
+        {
+            LoginViewModel l = new LoginViewModel();
+            l.Remember = true;
+            return View(l);
+        }
+
+        [HttpPost]
+        [Route("~/Login")]
+        public ActionResult Login(LoginViewModel m)
+        {
+            if (ModelState.IsValid)
+            {
+                if (m.Username == "XXXX" && m.Password == "XXXX")
+                {
+                    return RedirectToAction("index");
+                }
+                else
+                {
+                    return View(m);
+                }
+            }
+            else {
+                return View(m);
+            }
+            
+        }
+    }
+}
---
+++ Project02_Models/Models/ViewModels/LoginViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Project02_Models.Models.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [StringLength(10, MinimumLength =3, ErrorMessage =  "ERROR!!!!")]
+        //[Display(Name ="Enter username")]
+        [Display(Name = "UsernameText", ResourceType = typeof(Project02_Models.Resources.Models.LoginViewModel))]
+        public string Username { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+
+        public string Password { get; set; }
+
+        [Display(Name = "Remember me!!!")]
+
+        public bool Remember { get; set; }
+    }
+}
